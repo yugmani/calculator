6647e96c75957886f0e5e6a35cb0d97edaa67e78
Himalaya <yugmani@yahoo.com>
2020-07-31 23:33:49 -0700

    Created files

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+    // Your code here...
+    
+const numberEl = $(".number");
+const operatorEl = $(".operator");
+const equalEl = $(".equal");
+const clearEl = $(".clear");
+console.log(numberEl);
+numberEl.on("click", function(){
+    numberEl.forEach(number => {
+        let count = number.value;
+        // console.log(count);
+    });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    });
